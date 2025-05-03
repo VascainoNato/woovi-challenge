1915cb89216532afd7b47794457b704de66805de
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-02 22:52:39 -0300

    feat: added-footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,17 @@
 
 export default function Footer() {
     return (
-        <footer>
-            Footer
+        <footer className="h-20 border border-gray-200 bg-blue-900 items-center">
+            <div className="w-full flex items-center pr-20 pl-20 h-20 justify-between">
+                <div className="flex items-center">
+                    <h1 className="text-white transition duration-500 ease hover:scale-105 cursor-pointer">© 2025 Rafael Satyro.</h1>
+                </div>
+                <div className="flex items-center gap-6">
+                    <h1 className="text-white transition duration-500 ease hover:scale-105 cursor-pointer">Vídeo Explicativo</h1>
+                    <h1 className="text-white transition duration-500 ease hover:scale-105 cursor-pointer">Outros Projetos</h1>
+                    <h1 className="text-white transition duration-500 ease hover:scale-105 cursor-pointer">Contato</h1>
+                </div>
+            </div>
         </footer>
     );
   }
