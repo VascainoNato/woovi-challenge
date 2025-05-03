24d87fd5901bb9f417011744a6a7a17b6ffd0f42
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-02 22:27:15 -0300

    feat: added-header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image';
+import Logo from '../public/challenge.png';
 
 export default function Header() {
     return (
-        <header>
-            Header
+        <header className="h-22.5 text-white border border-gray-200">
+            <div className="flex items-center pr-20 pl-20 w-full">
+                <Image
+                    src={Logo}
+                    alt="Logo"
+                    className="w-30 transition duration-500 ease hover:scale-110"
+                > 
+                </Image>
+            </div>
         </header>
     );
-  }
+}
   
