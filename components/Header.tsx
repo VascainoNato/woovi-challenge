431fbe23f515ffc6203b12f97e01e3022cfedf8e
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-02 23:43:25 -0300

    fix: adjusted-header-to-mobile-first

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,21 @@ import Logo from '../public/challenge.png';
 
 export default function Header() {
     return (
-        <header className="h-20 text-white border border-gray-200">
-            <div className="flex items-center h-20 pr-20 pl-20 w-full">
-                <Image
-                    src={Logo}
-                    alt="Logo"
-                    className="w-30 transition duration-500 ease hover:scale-110"
-                > 
-                </Image>
+        <header className="h-35 text-white border border-gray-200 md:h-20">
+            <div className="flex flex-col items-center h-35 pr-4 pl-4 w-full gap-2 md:flex md:flex-row md:h-20 md:pl-10 md:pr-10 md:justify-between md:gap-0 xl:pr-20 xl:pl-20">
+                <div className="flex items-center">
+                    <Image
+                        src={Logo}
+                        alt="Logo"
+                        className="w-30 transition duration-[800ms] ease hover:scale-105"
+                    > 
+                    </Image>
+                </div>
+                <div className="flex items-center gap-6">
+                    <h1 className="text-blue-900 transition cursor-pointer animate-pulse-slow font-semibold">Challenge</h1>
+                    <h1 className="text-blue-900 transition duration-[800ms] ease hover:scale-105 cursor-pointer font-semibold">Vídeo Explicativo</h1>
+                    <h1 className="text-blue-900 transition duration-[800ms] ease hover:scale-105 cursor-pointer font-semibold">Github</h1>
+                </div>
             </div>
         </header>
     );
