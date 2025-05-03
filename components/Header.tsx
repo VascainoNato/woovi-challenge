4e1cfe8c61bda084bd3a134380ca650d29ed3a61
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-03 00:40:55 -0300

    feat: added-github-icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import Logo from '../public/challenge.png';
+import Github from '../public/github.png';
 import ChallengeModal from './ChallengeModal';
 
 export default function Header() {
@@ -9,7 +10,7 @@ export default function Header() {
         return (
             <>
             <header className="h-35 text-white border-b border-gray-200 md:h-20">
-                <div className="flex flex-col items-center h-35 pr-4 pl-4 w-full gap-2 md:flex md:flex-row md:h-20 md:pl-10 md:pr-10 md:justify-between md:gap-0 xl:pr-20 xl:pl-20">
+                <div className="flex flex-col items-center h-35 gap-2 pr-4 pl-4 w-full md:flex md:flex-row md:h-20 md:pl-10 md:pr-10 md:justify-between md:gap-0 xl:pr-20 xl:pl-20">
                     <div className="flex items-center">
                         <Image
                             src={Logo}
@@ -21,7 +22,13 @@ export default function Header() {
                     <div className="flex items-center gap-6">
                         <h1 className="text-blue-900 transition cursor-pointer animate-pulse-slow font-semibold" onClick={() => setShowModal(true)}>Challenge</h1>
                         <h1 className="text-blue-900 transition duration-[800ms] ease hover:scale-105 cursor-pointer font-semibold">Vídeo Explicativo</h1>
-                        <h1 className="text-blue-900 transition duration-[800ms] ease hover:scale-105 cursor-pointer font-semibold">Github</h1>
+                        <Image
+                            src={Github}
+                            alt="GitHub Logo"
+                            className='hidden md:flex md:w-12 transition duration-[800ms] ease hover:scale-105 cursor-pointer'
+                        >
+                        </Image>
+                        <h1 className="text-blue-900 transition duration-[800ms] ease hover:scale-105 cursor-pointer font-semibold md:hidden">Github</h1>
                     </div>
                 </div>
             </header>
