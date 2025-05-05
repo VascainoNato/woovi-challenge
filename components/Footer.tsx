f206729497d369cbc5597d469462e1119797e40d
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-05 17:26:15 -0300

    fix: adjust-anchor-links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,12 +4,16 @@ export default function Footer() {
         <footer className="h-25 border border-gray-200 bg-blue-900 items-center md:h-20">
             <div className="w-full flex flex-col items-center pr-4 pl-4 h-25 justify-center gap-4 md:flex-row md:h-20 md:pl-10 md:pr-10 md:justify-between md:gap-0 xl:pr-20 xl:pl-20">
                 <div className="flex items-center">
-                    <h1 className="text-white transition duration-[800ms] ease hover:scale-105 cursor-pointer">© 2025 Rafael Satyro.</h1>
+                    <a href="https://www.linkedin.com/in/rafael-pereira-satyro/" target="_blank" rel="noopener noreferrer">
+                        <h1 className="text-white transition duration-[800ms] ease hover:scale-105 cursor-pointer">© 2025 Rafael Satyro.</h1>
+                    </a>
                 </div>
                 <div className="flex items-center gap-6">
                     <h1 className="text-white transition duration-[800ms] ease hover:scale-105 cursor-pointer">Vídeo Explicativo</h1>
                     <h1 className="text-white transition duration-[800ms] ease hover:scale-105 cursor-pointer">Outros Projetos</h1>
-                    <h1 className="text-white transition duration-[800ms] ease hover:scale-105 cursor-pointer">Contato</h1>
+                    <a href="https://wa.me/+5567999850907" target="_blank" rel="noopener noreferrer">
+                        <h1 className="text-white transition duration-[800ms] ease hover:scale-105 cursor-pointer">Contato</h1>
+                    </a>
                 </div>
             </div>
         </footer>
