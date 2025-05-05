66aebabf6271cdf3e30bf0ced568673f2106f91a
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-05 19:28:18 -0300

    feat: added-use-create-node-hook

---
+++ hooks/useCreateNode.ts
@@ -0,0 +1,14 @@
+import { Node } from "react-flow-renderer";
+import { v4 as uuidv4 } from "uuid";
+
+export function useCreateNode() {
+  return (type: "webhook" | "api"): Node => ({
+    id: uuidv4(),
+    type,
+    position: {
+      x: Math.random() * 300,
+      y: Math.random() * 300,
+    },
+    data: {},
+  });
+}
