fc293b02cf90d55f122a9675ee9ed118565d1af2
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-05 19:50:55 -0300

    fix: custom-props-to-webhook-workflow

--- components/nodes/WebhookNode.tsx
+++ components/nodes/WebhookNode.tsx
@@ -2,13 +2,12 @@ import { Handle, Position } from "react-flow-renderer";
 
 export default function WebhookNode() {
   return (
-    <div className="p-3 bg-green-100 border border-green-600 rounded shadow-md text-sm min-w-[140px]">
-      <strong className="text-green-800">Webhook</strong>
-
+    <div className="p-3 woovie-color border border-gray-500 rounded shadow-md text-sm min-w-[140px]">
+      <strong className="text-white">Webhook</strong>
       <Handle
         type="source"
         position={Position.Right}
-        className="w-3 h-3 bg-green-600"
+        className="w-3 h-3 bg-blue-900 rounded-full"
       />
     </div>
   );
