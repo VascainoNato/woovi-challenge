d9e1664d05701e654b8715c276d8ab44bede2b96
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-05 21:17:27 -0300

    feat: added-delete-zustand-store

--- stores/useFlowStore.ts
+++ stores/useFlowStore.ts
@@ -2,13 +2,14 @@ import { Edge, Node } from "react-flow-renderer";
 import { create } from "zustand";
 
 interface FlowState {
-    nodes: Node[];
-    edges: Edge[];
-    setNodes: (nodes: Node[]) => void;
-    setEdges: (edges: Edge[] | ((prev: Edge[]) => Edge[])) => void;
-    addNode: (node: Node) => void;
-    addEdge: (edge: Edge) => void;
-    resetFlow: () => void;
+  nodes: Node[];
+  edges: Edge[];
+  setNodes: (nodes: Node[]) => void;
+  setEdges: (edges: Edge[] | ((prev: Edge[]) => Edge[])) => void;
+  addNode: (node: Node) => void;
+  addEdge: (edge: Edge) => void;
+  deleteNode: (id: string) => void;
+  resetFlow: () => void;
 }  
 
 export const useFlowStore = create<FlowState>((set) => ({
@@ -23,5 +24,13 @@ export const useFlowStore = create<FlowState>((set) => ({
     set((state) => ({ nodes: [...state.nodes, node] })),
   addEdge: (edge) =>
     set((state) => ({ edges: [...state.edges, edge] })),
+  deleteNode: (id) =>
+    set((state) => ({
+      nodes: state.nodes.filter((node) => node.id !== id),
+      edges: state.edges.filter(
+        (edge) => edge.source !== id && edge.target !== id
+      ),
+    })),
   resetFlow: () => set({ nodes: [], edges: [] }),
 }));
+
