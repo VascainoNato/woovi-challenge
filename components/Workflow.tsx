5e175c4640b8e2a42c6a94e57928671b98b6df75
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-05 23:24:01 -0300

    feat: updated-workflow-component

--- components/Workflow.tsx
+++ components/Workflow.tsx
@@ -2,31 +2,35 @@
 import ReactFlow, { addEdge, applyEdgeChanges, applyNodeChanges, Background, Connection, Controls, EdgeChange, MiniMap, NodeChange, ReactFlowProvider } from "react-flow-renderer";
 import { useFlowStore } from "../stores/useFlowStore";
 import { nodeTypes } from "../stores/nodeTypes";
+import { markEntryNodes, markFirstEntryNode } from "@/services/workflowService";
 import { useEffect } from "react";
 
 export default function Workflow() {
   const { nodes, edges, setNodes, setEdges } = useFlowStore();
 
-  const onNodesChange = (changes: NodeChange[]) =>
-    setNodes(applyNodeChanges(changes, nodes));
+  const onNodesChange = (changes: NodeChange[]) => {
+    const updatedNodes = applyNodeChanges(changes, nodes);
+    if (JSON.stringify(updatedNodes) !== JSON.stringify(nodes)) {
+      setNodes(updatedNodes);
+    }
+  };
 
-  const onEdgesChange = (changes: EdgeChange[]) =>
-    setEdges(applyEdgeChanges(changes, edges));
+  const onEdgesChange = (changes: EdgeChange[]) => {
+    const updatedEdges = applyEdgeChanges(changes, edges);
+    const updatedNodes = markEntryNodes(nodes, updatedEdges);
+    setEdges(updatedEdges); 
+    if (JSON.stringify(nodes) !== JSON.stringify(updatedNodes)) {
+      setNodes(updatedNodes);
+    }
+  };
 
   const onConnect = (connection: Connection) =>
     setEdges((prev) => addEdge(connection, prev));
 
   useEffect(() => {
-    const targets = edges.map((e) => e.target);
-    const updatedNodes = nodes.map((node) => ({
-      ...node,
-      data: {
-        ...node.data,
-        isEntry: !targets.includes(node.id),
-      },
-    }));
+    const updatedNodes = markFirstEntryNode(nodes, edges);
     setNodes(updatedNodes);
-  }, [edges]);
+  }, [edges, nodes.length]);
 
   return (
     <ReactFlowProvider>
