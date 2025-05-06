5e27fa8a16e7352bb49f0ae05f9acb4a4af2d775
rafaelsatyro <rafael.satyro@hotmail.com>
2025-05-05 21:16:52 -0300

    feat: added-custom-components-props

--- components/nodes/ApiNode.tsx
+++ components/nodes/ApiNode.tsx
@@ -2,15 +2,13 @@ import { Handle, Position } from "react-flow-renderer";
 
 export default function ApiNode() {
   return (
-    <div className="p-3 bg-blue-100 border border-blue-500 rounded shadow-md text-sm min-w-[120px]">
-      <strong className="text-blue-800">API</strong>
-
+    <div className="woovie-color border border-gray-500 rounded shadow-md min-w-[100px] pb-0.5">
+      <span className="text-white text-xs w-full pl-2">API</span>
       <Handle
         type="target"
         position={Position.Left}
         className="w-3 h-3 bg-blue-700"
       />
-
       <Handle
         type="source"
         position={Position.Right}
--- components/nodes/WebhookNode.tsx
+++ components/nodes/WebhookNode.tsx
@@ -1,9 +1,26 @@
 import { Handle, Position } from "react-flow-renderer";
+import { useFlowStore } from "../../stores/useFlowStore";
+import close from "../../public/close-white.png";
+import Image from "next/image";
+import { WebhookNodeProps } from "../../types/webhook";
 
-export default function WebhookNode() {
+export default function WebhookNode({ id }: WebhookNodeProps) {
+  const { deleteNode } = useFlowStore();
   return (
     <div className="woovie-color border border-gray-500 rounded shadow-md min-w-[100px] pb-0.5">
       <span className="text-white text-xs w-full pl-2">Webhook</span>
+      <button
+        onClick={() => deleteNode(id)}
+        className="absolute top-0 right-0.5 text-white rounded-bl"
+      >
+      <Image
+        src={close}
+        alt="Close"
+        className="w-4"
+      >
+      </Image>
+      
+      </button>
       <Handle
         type="source"
         position={Position.Right}
